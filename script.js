3b4589dd42bbcffa3ad1bc0ec29c152b40a68618
Teangelo Burks <burks.teangelo@gmail.com>
2021-01-09 09:56:11 -0500

    Removed some bootstrap code from HTML file that was not needed.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    //object of times
+    // Object with Hours/Hourly Events.
 
 var times = [
     { hour: 9, hourlyEvent: "" },
@@ -14,13 +14,11 @@ var times = [
     { hour: 17, hourlyEvent: "" },]
 
 
-    if(localStorage.getItem("savedInput")){ // "Sav"
+    if(localStorage.getItem("savedInput")){ // "savedInput: Names of things were saving to local storage.
         times = JSON.parse(localStorage.getItem("savedInput"));
     }
     
-
-
-    //Displaying Current Date
+   //Displaying Current Date
     function alwaysToday() {
         $("#currentDay").html(moment().format('MMMM Do YYYY, h:mm:ss a'));
     }
@@ -53,26 +51,20 @@ var times = [
         var saveBtn = $("<div>").attr("class", "col-md-1 saveBtn").attr("btn-id", times[i].hour);
         var icon = $("<i>").attr("class", "far fa-save");
 
-
-
+       // Adding Rows
         newRow.append(hourValue, inputValue, saveBtn);
         saveBtn.append(icon);
         $(".container").append(newRow)
-        // Setting up color coded blocks for past, current, and present hours.
-        // Past = White | Present = Tan | Future = Gray
 
     }
 
-
-
-
-    // Code for saving input to local storage
+// Code for saving input to local storage
     $(".saveBtn").on("click", function () {
         let btnId = $(this).attr("btn-id")
         let arrayIndex = times.findIndex(i=>i.hour==btnId) // Checking to see which index of array is matching hour.
         console.log(arrayIndex);
         times[arrayIndex].hourlyEvent = $("#" + btnId).val().trim()
-        localStorage.setItem("savedInput", JSON.stringify(times)) // JSON is saving array. JSON converts the array into a string.
+        localStorage.setItem("savedInput", JSON.stringify(times)) // JSON converts the array into a string.
        
 
     })
@@ -80,13 +72,3 @@ var times = [
 }
 
 );
-
-
-
-
-
-/*<div class="row">
-        <div class="col-md-1 hour pt-4">10am</div>
-        <input id="10Row" class="user-event-input col-md-10 event-input-block">
-        <div class="col-md-1 saveBtn" data-hour=10><i class="far fa-save" aria-hidden="true"></i></div>
-      </div> */
